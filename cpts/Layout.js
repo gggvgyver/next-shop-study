d28f3103c323a8a3ee126089645e2d1c01b643e0
gggvgyver <gggvgyver@gmail.com>
2022-06-24 18:50:36 +0900

    005레이아웃 확인

--- cpts/Layout.js
+++ cpts/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Head from 'next/head'
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>next-shop-study | 220622</title>
+        <meta name="description" content="학습용 next js 쇼핑몰" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <header>헤더</header>
-      <main>메인</main>
+      <main>{children}</main>
       <footer>푸더</footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../cpts/Layout'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>next-shop-study | 220622</title>
-        <meta name="description" content="학습용 next js 쇼핑몰" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1 className="text-3xl font-bold underline">
-        tailwind css 적용되는것을 확인했음
-      </h1>
-    </div>
-  )
+  return <Layout>홈페이지</Layout>
 }
